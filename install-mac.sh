d08920e2fec25cbca978114bb24f8b4979d7cce8
Jenny Plunkett <jenny@edgeimpulse.com>
2021-03-26 16:25:49 -0500

    Fix mac install script

--- install-mac.sh
+++ install-mac.sh
@@ -1,27 +1,43 @@
 #!/usr/bin/env bash
-if test ! $(which brew); then
-	echo "Installing homebrew..."
+LINUX="false"
+while :; do
+	case $1 in
+		-l|--install-ei-linux) LINUX="true"
+		;;
+		*) break
+	esac
+	shift
+done
+if [[ $(command -v brew) == "" ]]; then
+	echo "Installing Homebrew..."
 	curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+else
+	echo "Updating Homebrew..."
+	brew update
 fi
-if test ! $(which python2); then
+if [[ $(command -v python2) == "" ]]; then
 	echo "Installing Python 2.7..."
 	brew install python2
 fi
-if test ! $(which node); then
+if [[ $(command -v node) == "" ]]; then
 	echo "Installing Node.js & npm..."
 	brew install node
+else
+	echo "Checking Node.js & npm versions..."
 fi
-if test ! $(which edge-impulse-daemon); then
+if [[ $(command -v edge-impulse-daemon) == "" ]]; then
 	echo "Installing edge-impulse-cli..."
 	npm install -g edge-impulse-cli
 else
 	echo "Updating edge-impulse-cli..."
 	npm update -g edge-impulse-cli
 fi
-if test ! $(which edge-impulse-linux); then
-	echo "Installing edge-impulse-linux..."
-	npm install -g edge-impulse-linux
-else
-	echo "Updating edge-impulse-linux..."
-	npm update -g edge-impulse-linux
+if [[ $LINUX == "true" ]]; then
+	if [[ $(command -v edge-impulse-linux) == "" ]]; then
+		echo "Installing edge-impulse-linux..."
+		npm install -g edge-impulse-linux
+	else
+		echo "Updating edge-impulse-linux..."
+		npm update -g edge-impulse-linux
+	fi
 fi
