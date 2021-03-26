cc61b0d1543781192d8e22e75a0ed8274f37ec98
Jenny Plunkett <jenny@edgeimpulse.com>
2021-03-26 18:09:49 -0500

    Add python to linux/windows scripts

--- install-windows.ps1
+++ install-windows.ps1
@@ -1,6 +1,37 @@
 #Requires -RunAsAdministrator
-echo "Installing chocolatey..."
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
-echo "Installing Node.js & npm..."
-cinst nodejs.install
+$testchoco = powershell choco -v
+if(-not($testchoco)) {
+    echo "Installing chocolatey..."
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+else {
+    echo "Updating chocolatey..."
+    choco upgrade chocolatey
+}
+$testpython = powershell python --version
+if(-not($testpython)) {
+    echo "Installing Python..."
+    choco install python
+}
+else {
+    echo "Updating Python..."
+    choco upgrade python
+}
+$testnode = powershell node -v
+if(-not($testnode)) {
+    echo "Installing Node.js & npm..."
+    cinst nodejs.install
+}
+else {
+    echo "Updating Node.js & npm..."
+    choco upgrade nodejs.install
+}
+$testnode = powershell node -v
+if(-not($testnode)) {
+    echo "Installing Node.js & npm..."
+    cinst nodejs.install
+}
+else {
+    echo "Updating Node.js & npm..."
+    choco upgrade nodejs.install
+}
