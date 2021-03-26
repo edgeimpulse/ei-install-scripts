4c114f19a228955699cd983d5a0f4c01646b33ec
Jenny Plunkett <jenny@edgeimpulse.com>
2021-03-26 14:39:06 -0500

    Add linux install script

---
+++ install-linux.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+sudo apt-get update && sudo apt-get install -y curl gcc g++ make build-essential sox ffmpeg
+if ! [ -x "$(command -v node)" ]; then
+	echo "Installing Node.js & npm..."
+	curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+	sudo apt-get install -y nodejs
+	node -v
+fi
+NPM_PREFIX=$(npm config get prefix)
+echo "npm prefix = " $NPM_PREFIX
+if [ $NPM_PREFIX = '/usr/local' ] || [ $NPM_PREFIX = '/usr' ]; then
+	echo "Setting npm prefix to ~/.npm-global ..."
+	mkdir ~/.npm-global
+	npm config set prefix '~/.npm-global'
+	echo 'export PATH=~/.npm-global/bin:$PATH' >> ~/.profile
+fi
+if ! [ -x "$(command -v edge-impulse-daemon)" ]; then
+	echo "Installing edge-impulse-cli..."
+	npm install -g edge-impulse-cli
+else
+	echo "Updating edge-impulse-cli..."
+	npm update -g edge-impulse-cli
+fi
+if ! [ -x "$(command -v edge-impulse-linux)" ]; then
+	echo "Installing edge-impulse-linux..."
+	npm install -g edge-impulse-linux
+else
+	echo "Updating edge-impulse-linux..."
+	npm update -g edge-impulse-linux
+fi
